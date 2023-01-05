28db74369ed58c404064734a002c2d247d7ab835
builtbyjosh <pc.refuge@gmail.com>
2023-01-04 21:24:37 -0600

    fix css

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,16 +1,18 @@
-import { Button, Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
   const { id, title, price, description, images } = product;
 
   return (
-    <Card style={{ width: '18rem' }} id={id}>
-      <Card.Img variant="top" src={images[0]} />
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/product/${id}`}>
+        <Card.Img variant="top" src={images[0]} />
+      </Link>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Subtitle>${price}</Card.Subtitle>
         <Card.Text>{description}</Card.Text>
-        <Button variant="primary">Add to Cart</Button>
       </Card.Body>
     </Card>
   );
