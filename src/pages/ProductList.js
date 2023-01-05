e04e6dde5a9b8589fc52a60906507e6526e4510f
builtbyjosh <pc.refuge@gmail.com>
2023-01-04 21:24:55 -0600

    add store ref

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import {useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../components/Product';
-import products from '../productsData';
+import { useProductsStore } from '../store';
+import axios from 'axios';
 
 const ProductList = () => {
+  const products = useProductsStore((state)=> (state.products))
   console.log('products: ', products);
+
+  const fetchedProducts = async () => {
+    const res = await axios.get('https://api.escuelajs.co/api/v1/products')
+    return res.data
+  }
+
+  console.log('FETCHED PRODUCTS: ', fetchedProducts());
+
+  
   return (
     <div>
       <h1>Latest Products</h1>
       {products && (
         <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product.id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
       )}
-      
     </div>
   );
 };
